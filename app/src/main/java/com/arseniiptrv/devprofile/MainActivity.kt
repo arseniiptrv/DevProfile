ccdfacd0228daddca7584ddc1326a7c1f60b786e
Arsenii Petrov <arseniiptrv@gmail.com>
2019-11-05 16:40:22 +0200

    05/11, commit no.2 to master: test, no particular changes

--- app/src/main/java/com/arseniiptrv/devprofile/MainActivity.kt
+++ app/src/main/java/com/arseniiptrv/devprofile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.arseniiptrv.devprofile
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val roundedBitmap = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        roundedBitmap.cornerRadius = 33f
+        //roundedBitmap.isCircular = true
+        logo.setImageDrawable(roundedBitmap)
     }
 }
